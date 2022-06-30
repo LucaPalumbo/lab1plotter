ec6a0fcf5099a75f9d4cf1299292c2b40a0e09a6
Luca <43042873+LucaPalumbo@users.noreply.github.com>
2022-06-30 11:03:12 +0200

    Update flask_app.py
    aggiunto error handling e fixato typo

--- flask_app.py
+++ flask_app.py
@@ -17,10 +17,11 @@ def index():
     if request.method == 'POST':
         p = Plotter(request.form)
         output = p.handle_graph()
+        if "error" in output:
+            flash(output)
         return render_template("index.html", out=output )
 
         #return send_file('plot.png', mimetype='image/png')
-
 
 
 
@@ -41,7 +42,11 @@ class Plotter(object):
         }
 
     def parse_data(self,data):
-        return np.array( json.loads(data) )
+        try:
+            array =  np.array( json.loads(data) )
+            return array
+        except:
+            return False
 
     def scatter(self, fig ,x_data, y_data, sigma_x, sigma_y):
         plt.errorbar(x_data, y_data, sigma_y, sigma_x, fmt='.')
@@ -74,7 +79,7 @@ class Plotter(object):
         return output
 
 
-    def compute_chi_sqared(self,modello, popt, x_data, y_data, sigma_y):
+    def compute_chi_squared(self,modello, popt, x_data, y_data, sigma_y):
         expected = modello(x_data,*popt)
         return (((expected-y_data)/sigma_y)**2).sum()
 
@@ -84,10 +89,22 @@ class Plotter(object):
         print( self.form )
 
         self.x_data = self.parse_data( self.form['x_data'] )
+        if type(self.x_data) != np.ndarray:
+            return "error: C'è un errore nei valori dell'asse x"
         self.y_data = self.parse_data( self.form['y_data'] )
+        if type(self.y_data) != np.ndarray:
+            return "error: C'è un errore nei valori dell'asse y"
         self.sigma_x = self.parse_data( self.form['sigma_x'] )
+        if type(self.sigma_x) != np.ndarray:
+            return "error: C'è un errore negli errori dell'asse x"
         self.sigma_y = self.parse_data( self.form['sigma_y'] )
+        if type(self.sigma_y) != np.ndarray:
+            return "error: C'è un errore negli errori dell'asse y"
 
+
+        result = all(el == len(self.x_data) for el in [len(self.x_data),len(self.y_data),len(self.sigma_x),len(self.sigma_y)] )
+        if result == False:
+            return "error: le liste devono essere tutte della stassa lunghezza"
 
         if (self.form['x_label'] != '' ):
             self.setAxisLabel(self.fig,self.form['x_label'],'x')
@@ -99,9 +116,12 @@ class Plotter(object):
         popt, sigmas = self.doFit(self.modelli[self.form['modello']] , self.x_data, self.y_data, self.sigma_y)
         self.drawBestFit(self.modelli[self.form['modello']],popt,  self.x_data)
 
-        x2 = self.compute_chi_sqared(self.modelli[self.form['modello']], popt, self.x_data, self.y_data, self.sigma_y)
+        x2 = self.compute_chi_squared(self.modelli[self.form['modello']], popt, self.x_data, self.y_data, self.sigma_y)
         output = self.getOutString(popt, sigmas, x2)
 
-        plt.savefig("./static/plot.png")
+        plt.savefig("/home/lucapalumbo/lab1plotter/static/plot.png")#/home/lucapalumbo/lab1plotter/static/plot.png
+
+        return output
+
 
         return output
